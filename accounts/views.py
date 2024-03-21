40ae26e2d5c70a11ccc3d8019cf4692d672f061d
jelenu <jesusleon2700@gmail.com>
2024-03-21 13:56:08 +0100

    Account info view modified

--- accounts/views.py
+++ accounts/views.py
@@ -11,23 +11,36 @@ class AccountInfo(APIView):
         # Get queryset parameters from the frontend
         game_name = request.query_params.get('gameName', None)
         tagline = request.query_params.get('tagLine', None)
+        server = request.query_params.get('server', None)
 
         # Check if gameName and tagline are present
         if not (game_name and tagline):
             return Response({'error': 'gameName and tagline are required'}, status=400)
 
-        # Build the header
+        # Build the header for the first API call
         headers = {
             'X-Riot-Token': os.getenv('TOKEN'),
         }
 
-        # Make a call to the Riot API
+        # Make a call to the Riot API for the first endpoint
         api_url = f'https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tagline}'
         response = requests.get(api_url, headers=headers)
 
         # Check if the API call was successful
         if response.status_code == 200:
             data = response.json()
-            return Response(data)
+            encrypted_puuid = data.get('puuid')
+            
+            # Make a call to the Riot API for the second endpoint
+            second_api_url = f'https://{server}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{encrypted_puuid}'
+            second_response = requests.get(second_api_url, headers=headers)
+            
+            # Check if the second API call was successful
+            if second_response.status_code == 200:
+                second_data = second_response.json()
+                
+                return Response(second_data)
+            else:
+                return Response({'error': 'Failed to fetch data from second API call'}, status=second_response.status_code)
         else:
-            return Response({'error': 'Failed to fetch data from third-party API'}, status=response.status_code)
+            return Response({'error': 'Failed to fetch data from first API call'}, status=response.status_code)
